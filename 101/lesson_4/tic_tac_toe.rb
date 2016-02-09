cb7545f54cbba8ff9ea81a58188185fbe1cbabcc
Roedet Samba <roedet@gmail.com>
2016-02-09 01:48:13 +0000

    update tictactoe robucop

--- 101/lesson_4/tic_tac_toe.rb
+++ 101/lesson_4/tic_tac_toe.rb
@@ -8,42 +8,43 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/MethodLength, Metrics/AbcSize
 def display_board(brd)
   system 'clear'
   puts " You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
-  puts ""
-  puts "     |     |"
+  puts ''
+  puts '     |     |'
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-  puts "     |     |"
-  puts "-----+-----+-----"
-  puts "     |     |"
+  puts '     |     |'
+  puts '-----+-----+-----'
+  puts '     |     |'
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
-  puts "     |     |"
-  puts ""
+  puts '     |     |'
+  puts ''
 end
+# rubocop:enable Metrics/MethodLength, Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_place_piece!(brd)
-  
   square = ''
   loop do
     prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
-    prompt " Sorry, that is not a valid choice"
+    prompt 'Sorry, that is not a valid choice'
   end
   brd[square] = PLAYER_MARKER
 end
@@ -65,46 +66,43 @@ def detect_winner(brd)
   winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
                   [[1, 5, 9], [3, 5, 7]]
-  
   winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-       brd[line[1]] == COMPUTER_MARKER &&
-       brd[line[2]] == COMPUTER_MARKER
-      return 'Computer'
+    # if brd[line[0]] == PLAYER_MARKER &&
+    #   brd[line[1]] == PLAYER_MARKER &&
+    #   brd[line[2]] == PLAYER_MARKER
+    #   return 'Player'
+    # elsif brd[line[0]] == COMPUTER_MARKER &&
+    #       brd[line[1]] == COMPUTER_MARKER &&
+    #       brd[line[2]] == COMPUTER_MARKER
+    #   return 'Computer'
+    # end
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return ' Computer'
     end
   end
   nil
 end
 
-
 loop do
   board = initialize_board
-  
   loop do
     display_board(board)
-    
     player_place_piece!(board)
     break if someone_won?(board) || board_full?(board)
-    
     computer_place_piece!(board)
     break if someone_won?(board) || board_full?(board)
   end
-  
   display_board(board)
-  
   if someone_won?(board)
     prompt "#{detect_winner(board)} has won"
   else
-    prompt "it is a tie"
+    prompt 'it is a tie'
   end
-  
-  prompt "Play again y/n?"
+  prompt 'Play again y/n?'
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt " Thanks for playing"
+prompt "Thanks for playing"
